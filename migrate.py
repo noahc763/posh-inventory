68e17e8a07297a6a4788a4cae465e73da464460e
Noah Caldon <noahcaldon@Noahs-MacBook-Air.local>
2025-09-03 22:03:12 -0400

    Fixed db issues

--- migrate.py
+++ migrate.py
@@ -2,6 +2,7 @@
 from flask_migrate import Migrate
 from models import db
 from app import create_app   # uses your existing factory
+from alembic import op
 
 app = create_app()
 migrate = Migrate(app, db)
@@ -10,3 +11,9 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db}
+
+def upgrade():
+    op.alter_column('items', 'listing_price_price', new_column_name='listing_price')
+
+def downgrade():
+    op.alter_column('items', 'listing_price', new_column_name='listing_price_price')
---
+++ migrations/versions/3f077dbc8586_rename_listing_price_price_to_listing_.py
@@ -0,0 +1,24 @@
+"""rename listing_price_price to listing_price
+
+Revision ID: 3f077dbc8586
+Revises: 915e45452d9d
+Create Date: 2025-09-03 22:00:13.283127
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3f077dbc8586'
+down_revision = '915e45452d9d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
